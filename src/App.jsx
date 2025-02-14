a486a54dd6eb45b1de8709a35049e406545d4514
Juan Martin Lavalle <lavallejuanmartin@gmail.com>
2025-02-14 17:02:24 -0300

    Start the project in dev branch

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <h1>Medicin Cal</h1>
+        <p>
+          Medicin Cal allows you to add medications with reminders and link them
+          to the calendar for their application.
+        </p>
+      </main>
+    </>
+  );
+}
+
+export default App;
